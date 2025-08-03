14148708d393688b8b2175a8589809a5295361ba
everybody-ah <fiorenicolas2005@gmail.com>
2025-08-02 21:14:18 -0400

    short day today but got started on chart command tommorow hopefully

---
+++ commands/chart.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js'); 
+const yahooFinance = require('yahoo-finance2').default; 
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chart')
+        .setDescription('Enter a symbol and get its charted history back')
+        .addStringOption(option => 
+            option.setName('symbol')
+                .setDescription('Search by Symbol')
+                .setRequired(true)),
+    async execute(interaction) {
+        const ticker = interaction.options.getString('symbol');
+
+        async function getStockHistory(symbol) { 
+            try{ 
+            const quereyOption = { period1: '2021-02-01', interval: '1d'}
+            const result = await yahooFinance.chart(symbol, quereyOption); 
+
+            return result;
+
+            } catch (error) { 
+                await interaction.reply(`The error was ${error.message}`);
+            }
+        }
+        getStockHistory('AAPL').then(data =>  {
+            console.log(data); 
+        });
+
+
+    },
+};
